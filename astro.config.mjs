762704684781f9d56d286a79a4de2431086fe426
yzh990918 <251205668@qq.com>
2023-03-23 16:58:42 +0800

    feat(pwa): add pwa support

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import unocss from 'unocss/astro'
 import solidJs from '@astrojs/solid-js'
 
 import node from '@astrojs/node'
+import astroPWA from '@vite-pwa/astro'
 import vercel from '@astrojs/vercel/edge'
 import netlify from '@astrojs/netlify/edge-functions'
 import vercelDisableBlocks from './plugins/vercelDisableBlocks'
@@ -22,6 +23,45 @@ const envAdapter = () => {
 // https://astro.build/config
 export default defineConfig({
   integrations: [
+    astroPWA({
+      includeAssets: ['favicon.svg'],
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ChatGPT-API Demo',
+        short_name: 'ChatGPT-API Demo',
+        description: 'A demo repo based on OpenAI GPT-3.5 Turbo API',
+        theme_color: '#212129',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+      workbox: {
+        navigateFallback: '/',
+      },
+      client: {
+        installPrompt: true,
+        periodicSyncForUpdates: 20,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
     unocss(),
     solidJs(),
   ],
